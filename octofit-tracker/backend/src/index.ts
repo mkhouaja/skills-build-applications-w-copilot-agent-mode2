ab9772ae1bd00c360f0dc2a3e95ade16883d337f
mkhouaja <manel.khouaja@gmail.com>
2026-06-16 12:28:11 +0000

    /init-populate-octofit_db

--- octofit-tracker/backend/src/index.ts
+++ octofit-tracker/backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import apiRouter from './routes';
+import apiRouter from './routes.js';
 
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
